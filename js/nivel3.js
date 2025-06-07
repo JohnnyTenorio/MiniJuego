c2e6b135f389274adcaa4c3146b2ded5ea6cd40e
JohnnyTenorio <125750190+JohnnyTenorio@users.noreply.github.com>
2025-06-07 11:16:15 -0500

    Update nivel3.js

--- js/nivel3.js
+++ js/nivel3.js
@@ -97,7 +97,7 @@ function mover(dx, dy) {
                 if (segundos === 0) {
                     clearInterval(intervalo);
                     // Redirigir a la página con el mensaje
-                    window.location.href = "mensajeFinal.html";  // <-- Cambia por la ruta correcta si quieres
+                    window.location.href = "/MiniJuego/html/mensajeFinal.html";  // <-- Cambia por la ruta correcta si quieres
                 }
             }, 1000);
         }
