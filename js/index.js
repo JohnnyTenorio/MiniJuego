2aefcaa4ee39e252cff67f7d67ec15d829cf6a71
Johnny Tenorio <jhonnytenorio33@gmail.com>
2025-06-07 10:02:06 -0500

    ACTUALIZACION

--- js/index.js
+++ js/index.js
@@ -11,10 +11,31 @@ function avanzar() {
     } else {
       document.getElementById("juego").classList.add("oculto");
       document.getElementById("final").classList.remove("oculto");
+      return;
+    }
+  }
+
+  let mensaje = `Nivel ${nivel} - Subnivel ${subnivel}`;
+  if (subnivel === 5) {
+    mensaje += " (Boss 🐉)";
+  }
+
+  document.getElementById("mensaje").textContent = mensaje;
+}
+function avanzar() {
+  if (subnivel < 5) {
+    subnivel++;
+  } else {
+    if (nivel < 3) {
+      nivel++;
+      subnivel = 1;
+    } else {
+      document.getElementById("juego").classList.add("oculto");
+      document.getElementById("final").classList.remove("oculto");
 
       // Esperar 3 segundos y luego redirigir
       setTimeout(() => {
-        window.location.href = "/MiniJuego/html/index.html"; // Revisa esta ruta también
+        window.location.href = "../html/index.html"; // Cambia esto por la página destino que quieras
       }, 5000);
 
       return;
@@ -28,3 +49,4 @@ function avanzar() {
 
   document.getElementById("mensaje").textContent = mensaje;
 }
+
